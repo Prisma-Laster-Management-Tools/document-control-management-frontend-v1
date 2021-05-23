67d47485d6ac44874b5dbb261c2b4ce92875245b
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-23 21:43:56 +0700

    initiate loader on app starts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Loader } from './core/loader';
+
+//
+// ─── LOADER ─────────────────────────────────────────────────────────────────────
+//
+Loader.init()
+// ────────────────────────────────────────────────────────────────────────────────
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
