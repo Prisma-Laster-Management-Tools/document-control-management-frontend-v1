da09e56476411a06da7589419a18c41792e41827
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-26 00:11:54 +0700

    instanciated authentication recoil in the app.js

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilState } from 'recoil';
 import Login from './components/pages/unauthorized/Authentication/views/Login';
 // import ClassWithRecoil from './components/pages/playground/ClassWithRecoil';
 import LandingPage from './components/pages/unauthorized/LandingPage';
+import { Authentication } from './core/authentication/Authentication';
 import { CreateClassWithEmbeddedState } from './core/recoil/transformation/class-with-embedded-state';
+import { authenticationState } from './store/recoil/authentication/authentication.atom';
 
 function App() {
+  //
+  // ─── STORING RECOIL REF ─────────────────────────────────────────────────────────
+  //
+  const [authState,setAuthState] = useRecoilState(authenticationState)
+  React.useEffect(() => {
+    Authentication.instanciate_recoil(authState,setAuthState)
+  }, [authState])
+  // ────────────────────────────────────────────────────────────────────────────────
+
+
   return (
-    <RecoilRoot>
       <Router>
           <Switch>
             <Route path="/" exact component={LandingPage}/>
             <Route path="/login" exact component={Login}/>
           </Switch>
       </Router>
-    </RecoilRoot>
   );
 }
 
