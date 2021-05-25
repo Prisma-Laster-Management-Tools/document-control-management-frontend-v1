89ba49df049ba01cdef73bd1742566804120b80b
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-26 02:41:27 +0700

    runs instanciate_recoil for Loading screen in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,40 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
+import FullLoadingScreen from './components/common/full-loading-screen';
 import Login from './components/pages/unauthorized/Authentication/views/Login';
 // import ClassWithRecoil from './components/pages/playground/ClassWithRecoil';
 import LandingPage from './components/pages/unauthorized/LandingPage';
 import { Authentication } from './core/authentication/Authentication';
+import { LoadingScreen } from './core/loading-screen/LoadingScreen';
 import { CreateClassWithEmbeddedState } from './core/recoil/transformation/class-with-embedded-state';
 import { authenticationState } from './store/recoil/authentication/authentication.atom';
+import { loadingScreenState } from './store/recoil/loading-screen/loading-screen.atom';
 
 function App() {
   //
   // ─── STORING RECOIL REF ─────────────────────────────────────────────────────────
   //
   const [authState,setAuthState] = useRecoilState(authenticationState)
+  const setLoadingScreenState = useSetRecoilState(loadingScreenState)
   React.useEffect(() => {
     Authentication.instanciate_recoil(authState,setAuthState)
   }, [authState])
+  React.useEffect(() => {
+    Authentication.load_stored_token_in_the_local_storage()
+
+    // Instantiate recoil [SETTER ONLY]
+    LoadingScreen.instanciate_recoil(setLoadingScreenState)
+    // ─────────────────────────────────────────────────────────────────
+
+  },[])
   // ────────────────────────────────────────────────────────────────────────────────
 
 
   return (
       <Router>
+          <FullLoadingScreen/>
           <Switch>
             <Route path="/" exact component={LandingPage}/>
             <Route path="/login" exact component={Login}/>
