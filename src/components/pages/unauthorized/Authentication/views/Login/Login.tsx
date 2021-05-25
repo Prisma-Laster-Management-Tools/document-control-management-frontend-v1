f169989e364a9d0ec4e56ba57df74462407393a3
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-25 18:26:55 +0700

    finalized login flow logic (remained-storing token part)

--- src/components/pages/unauthorized/Authentication/views/Login/Login.tsx
+++ src/components/pages/unauthorized/Authentication/views/Login/Login.tsx
@@ -1,20 +1,95 @@
 import React from 'react'
+import API_instance from '../../../../../../core/axios/instance'
 import Navbar from '../../../../../common/navbar'
+import { API_Login } from '../../apis/authentication'
 import { BgContainer, InputValue, LoginBtn, LoginContainer, LoginText, TextP } from './login.styles'
 
-export default function Login() {
+import { Typography, message } from 'antd';
+import LoadingIndicator from '../../../../../common/loading-indicator'
+import { sleep } from '../../../../../../utilities/fake-loader/fakeLoader'
+const { Text, Link } = Typography;
+
+interface ICredentialInput{
+    email: string
+    password:string
+}
+
+
+// TODO Refactor the onChange function later to be more dynamic
+export default function Login(props:any) {
+    const [credentialInput,setCredentialInput] = React.useState<ICredentialInput>({email:'',password:''})
+    const [errors,setErrors] = React.useState<any>({})
+    const [isLoading,setIsLoading] = React.useState<boolean>(false)
+
+    function clear_errors(){
+        setErrors({})
+    }
+
+
+    // FOR VISUALIZATION UI
+    function UI_RenderErrorText(pointer: keyof ICredentialInput){
+        if(pointer in errors){
+            // if the error exist
+            return <Text type="danger">{errors[pointer]}</Text>
+        }else{
+            return null
+        }
+    }
+
+    //PREFAB
+    const rendered_confirm_text = isLoading ? <LoadingIndicator size="large"/> : "ลงชื่อเข้าใช้"
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+
+    async function onLoggingIn(){
+        console.log("Logging in")
+        setIsLoading(true)
+        await sleep(1150)
+        const response = await API_Login(credentialInput.email,credentialInput.password)
+        setIsLoading(false)
+        if(response.success === false){
+           // error happenning
+           if(response.error_type === 'validation'){
+               // validation error goes here
+               console.log('validation error')
+               console.log(response.data.errors)
+               setErrors(response.data.errors)
+           }else if(response.error_type === 'authorization'){
+            clear_errors()
+            message.error("อีเมล์ หรือ รหัสผ่าน ไม่ถูกต้อง",2.5)
+           }
+        }else{
+            clear_errors()
+            const {accessToken} = response.data
+            console.log(accessToken)
+            // TODO store token in the the local storage and also store in the authentication recoil
+
+            message.success("ลงชื่อเข้าใช้สำเร็จ",1.5)
+            await sleep(1500)
+            props.history.push('/dashboard')
+        }
+    }
     return (
         <>
             <Navbar/>
             <BgContainer/>
             <LoginContainer>
                 <LoginText>ลงชื่อเข้าใช้</LoginText>
-                <TextP fontSize={24}>ไอดี</TextP>
-                <InputValue></InputValue>
+                <TextP fontSize={24}>อีเมล์</TextP>
+                <InputValue value={credentialInput.email} onChange={(event) => {
+                    setCredentialInput(prevState => ({...prevState,email:event.target.value}))
+                }}/>
+                {UI_RenderErrorText('email')}
                 <TextP fontSize={24}>รหัสผ่าน</TextP>
-                <InputValue></InputValue>
+                <InputValue onChange={(event) => {
+                    setCredentialInput(prevState => ({...prevState,password:event.target.value}))
+                }}/>
+                {UI_RenderErrorText('password')}
                 <TextP fontSize={18}>ไอดี คือที่อยู่อีเมลที่คุณใช้ยืนยันตัวตนกับบริษัทแล้วเพื่อใช้ เข้าสู่ระบบ</TextP>
-                <LoginBtn>ลงชื่อเข้าใช้</LoginBtn>
+                <LoginBtn onClick={() => {
+                    onLoggingIn()
+                }}>{rendered_confirm_text}</LoginBtn>
             </LoginContainer>
         </>
     )
--- src/components/pages/unauthorized/Authentication/views/Login/login.styles.ts
+++ src/components/pages/unauthorized/Authentication/views/Login/login.styles.ts
@@ -1,131 +1,129 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 import { Button } from 'antd';
-import bg_image from '../../../../../../assets/main_background.jpg'
+import bg_image from '../../../../../../assets/main_background.jpg';
 
 ///// Background /////
 export const BgContainer = styled.div`
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    background-image: url(${bg_image});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    z-index: -1;
-`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bg_image});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: -1;
+`;
 
 //// Other /////
 export const LoginContainer = styled.div`
-    /* LoginContainer */
-    /* Auto Layout */
-    width: 100%;
-    height: 100vh;
+  /* LoginContainer */
+  /* Auto Layout */
+  width: 100%;
+  height: 100vh;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    align-content: center;
-    padding: 80px 280px 0px 280px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: center;
+  padding: 80px 280px 0px 280px;
+`;
 export const LoginText = styled.h1`
-    /* ลงชื่อเข้าใช้ */
-    
-    width: 400px;
-    height: 57px;
+  /* ลงชื่อเข้าใช้ */
 
-    font-family: 'Kanit', sans-serif;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 48px;
-    line-height: 24px;
-    /* or 50% */
+  width: 400px;
+  height: 57px;
 
-    color: #444444;
-    /* Inside Auto Layout */
+  font-family: 'Kanit', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 24px;
+  /* or 50% */
 
-    flex: none;
-    order: 0;
-    flex-grow: 0;
-    margin-bottom: 30px;
-`
-export const TextP = styled.p<{fontSize: number}>`
-    /* ไอดี */
-    width: 420px;
-    height: 48px;
+  color: #444444;
+  /* Inside Auto Layout */
 
-    font-family: 'Kanit', sans-serif;
-    font-style: normal;
-    font-weight: 600;
-    font-size: ${props=>props.fontSize}px;
-    line-height: 24px;
-    /* or 100% */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin-bottom: 30px;
+`;
+export const TextP = styled.p<{ fontSize: number }>`
+  /* ไอดี */
+  width: 420px;
+  height: 48px;
 
-    display: flex;
-    align-items: center;
+  font-family: 'Kanit', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: ${(props) => props.fontSize}px;
+  line-height: 24px;
+  /* or 100% */
 
-    color: #707070;
+  display: flex;
+  align-items: center;
 
+  color: #707070;
 
-    /* Inside Auto Layout */
+  /* Inside Auto Layout */
 
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-    margin: 5px 10px;
-`
+  flex: none;
+  //order: 1;
+  flex-grow: 0;
+  margin: 5px 10px;
+`;
 export const InputValue = styled.input`
-    /* Rectangle 1 */
+  /* Rectangle 1 */
 
-    position: static;
-    width: 440px;
-    height: 65px;
-    left: 280px;
-    top: 369px;
+  width: 440px;
+  height: 65px;
+  left: 280px;
+  top: 369px;
 
-    border: 3px solid #F2C94C;
-    border-radius: 20px;
+  border: 3px solid #f2c94c;
+  border-radius: 20px;
 
-    /* Inside Auto Layout */
+  /* Inside Auto Layout */
 
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-    margin: 0px 0px;
-`
+  flex: none;
+  //order: 1;
+  flex-grow: 0;
+  margin: 0px 0px;
+`;
 export const LoginBtn = styled(Button)`
-    /* Login Button 1 */
-    width: 440px;
-    height: 65px;
+  /* Login Button 1 */
+  width: 440px;
+  height: 65px;
 
-    /* Inside Auto Layout */
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-    margin: 50px 0px;
-    border-radius: 20px;
-    
-    background: #F2C94C;
-    border: 3px solid #F2C94C;
-    border-radius: 20px;
+  /* Inside Auto Layout */
+  flex: none;
+  //order: 1;
+  flex-grow: 0;
+  margin: 50px 0px;
+  border-radius: 20px;
 
-    font-family: 'Kanit', sans-serif;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 24px;
-    line-height: 24px;
+  background: #f2c94c;
+  border: 3px solid #f2c94c;
+  border-radius: 20px;
+
+  font-family: 'Kanit', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 24px;
+  color: white;
+  /* or 100% */
+  &:hover {
+    background-color: gray;
     color: white;
-    /* or 100% */
-    &:hover{
-        background-color: gray;
-        color: white;
-        border: none;
-        box-shadow: none;
-    }
-    &:focus{
-        background-color: gray;
-        color: white;
-        border: none;
-        box-shadow: none;
-    }
-`
+    border: none;
+    box-shadow: none;
+  }
+  &:focus {
+    background-color: gray;
+    color: white;
+    border: none;
+    box-shadow: none;
+  }
+`;
