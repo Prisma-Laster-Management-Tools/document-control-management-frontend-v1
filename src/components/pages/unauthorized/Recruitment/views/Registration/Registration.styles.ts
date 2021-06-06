fee0fdf9cd833541bbe2fdd7a6351dfccd05065e
unknown <tanawatt2541@gmail.com>
2021-06-06 23:02:12 +0700

    updated: registration styles

--- src/components/pages/unauthorized/Recruitment/views/Registration/Registration.styles.ts
+++ src/components/pages/unauthorized/Recruitment/views/Registration/Registration.styles.ts
@@ -1,2 +1,13 @@
 import styled from '@emotion/styled'
 import { Button } from 'antd';
+
+export const RegisBGMain = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #d9d9d9;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
--- src/components/pages/unauthorized/Recruitment/views/Registration/Registration.tsx
+++ src/components/pages/unauthorized/Recruitment/views/Registration/Registration.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { RegisBGMain } from './Registration.styles'
 
 export default function Registration() {
     return (
         <>
-            Hello
+            <RegisBGMain>
+                <div>Text</div>
+                <div>f name</div>
+                <div>l name</div>
+                <div>position</div>
+
+                <div>e-mail</div>
+                <div>password</div>
+                <div>re-password</div>
+
+                <div>Button</div>
+            </RegisBGMain>
         </>
     )
 }
