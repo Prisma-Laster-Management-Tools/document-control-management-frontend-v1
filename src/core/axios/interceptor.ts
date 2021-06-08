97d916816249109b3bf3e07a67a16f0e93d15a24
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-08 19:04:38 +0700

    :fire: removed logging in the interceptor

--- src/core/axios/interceptor.ts
+++ src/core/axios/interceptor.ts
@@ -5,9 +5,7 @@ API_Instance.interceptors.request.use(async function (config) {
     /*config.timeout = parseInt(
       getEnvironmentVariable('AXIOS_TIMEOUT_DURATION') as string
     );*/
-    console.log('clear');
     const token = LocalStorage.get_data_from_key('token');
-    console.log(token);
     config.headers['Authorization'] = 'Bearer ' + token;
     return config;
 });
