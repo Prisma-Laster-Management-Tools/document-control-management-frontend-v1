3983ffef49f8491a78a4ac9818f2b77db84bc2e9
unknown <tanawatt2541@gmail.com>
2021-06-09 20:24:46 +0700

    updated: Dashboard style for rescaling screen

--- src/components/pages/authorized/Dashboard/views/dashboard.styles.ts
+++ src/components/pages/authorized/Dashboard/views/dashboard.styles.ts
@@ -15,6 +15,7 @@ export const BgContainer = styled.div`
 export const DashboardMainContainer = styled.div`
     position: relative;
     width: 100%;
+    height: 90vh;
     opacity: 1;
     padding: 0px 150px 0px 150px;
 
@@ -22,19 +23,27 @@ export const DashboardMainContainer = styled.div`
     
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: flex-start;
     
     font-family: 'Kanit', sans-serif;
     font-style: normal;
+    @media (max-width:1400px){
+        padding: 0px 80px 0px 80px;
+    }
+    @media (max-width:1200px){
+        padding: 0px 50px 0px 50px;
+    }
+    transition: background-color 1s ;
 `
 
 ///////// MenuLeft /////////
 export const MenuLeft = styled.div`
-    width: 20rem;
-    //height: 100vh;
+    width: 15%;
+    height: 100%;
     //background: rgba(196, 196, 196, 0.5);
     padding-top: 60px;
+    //background-color: white;
 `
 export const MenuDivInner =  styled.div`
     display:flex;
@@ -43,17 +52,17 @@ export const MenuDivInner =  styled.div`
     align-items: center;
     width: 100%;
     height: 100%;
-    //background-color: gray;
+    //background-color: white;
 `
 export const MenuDivButton =  styled.div`
     display:flex;
     flex-direction: row;
     justify-content: space-evenly;
     align-items: center;
-    width: 80%;
+    width:90%;
     height: 50px;
     border-radius: 10px;
-    //background-color: green;
+    background-color: white;
     margin-bottom: 20px;
     &:hover {
         background-color: #d9d9d9;
@@ -73,23 +82,28 @@ export const MenuDivButton =  styled.div`
     transition: background-color 0.2s ;
 `
 
-export const MenuButtonText = styled.p`
-    width: 50%;
-    margin-top: 24px;
+export const MenuButtonText = styled.div`
+    width: 60%;
     font-family: 'Kanit', sans-serif;
     font-style: normal;
-    font-size: 25px;
+    font-size: 20px;
     color: #555555;
     user-select: none;
     //border: 1px solid #555;
+    @media (max-width:1575px){
+        font-size: 16px;
+    }
+    @media (max-width:1200px){
+        font-size: 14px;
+    }
 `
 //////////// Mid body /////////////
 
 export const MidBody = styled.div`
     padding: 60px 20px 20px 20px;
-    width: 920px;
-    //height: 55rem;
-    //background-color: rgba(97, 208, 255, 0.2);
+    width: 60%;
+    height: 100%;
+    background-color: #f9f9f9;
 
     display: flex;
     flex-direction: column;
@@ -98,14 +112,15 @@ export const MidBody = styled.div`
 
     font-family: 'Kanit', sans-serif;
     font-size: 15px;
+    border-radius: 0rem 0rem 1rem 1rem;
     //border: 1px solid black;
 `
 export const MidTopContainer = styled.div`
     width: 100%;
-    height: 10vh;
+    //height: 10%;
     padding: 20px 20px 20px 20px;
 
-    //background-color: pink;
+    
 
     display: flex;
     flex-direction: row;
@@ -116,7 +131,7 @@ export const MidTopContainer = styled.div`
 `
 export const MidMainContainer = styled.div`
     width: 100%;
-    height: 65vh;
+    height: 60vh;
     padding: 20px 20px 20px 20px;
     
     //background-color: purple;
@@ -140,17 +155,24 @@ export const NameText = styled.p`
     margin: 1px 0px 0px 10px;
     padding-left: 10px;
     font-size: 20px;
+    @media (max-width:1575px){
+        font-size: 18px;
+    }
+    @media (max-width:1200px){
+        font-size: 16px;
+    }
 `
 
 export const GoTaskBtn = styled(Button)`
     margin-top: 20px;
-    width: 400px;
-    height: 50px;
+    width: 50%;
+    height: 8%;
     background-color: #454545;
     color: white;
     box-shadow: none;
     border-radius: 10px;
     border: none;
+    font-size: 18px;
     &:hover{
         background-color: gray;
         color: white;
@@ -163,8 +185,12 @@ export const GoTaskBtn = styled(Button)`
         border: none;
         box-shadow: none;
     }
-
-    
+    @media (max-width:1575px){
+        font-size: 16px;
+    }
+    @media (max-width:1200px){
+        font-size: 14px;
+    }
 `
 /////////   Top DropDown  ////////
 export const SelectDropDown = styled(Dropdown)`
@@ -194,31 +220,39 @@ export const NotificationContainer = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-    align-self: flex-start;
-    padding: 60px 10px 10px;
+    padding: 50px 10px 10px;
 
-    background: white;
+    //border: 1px solid blue;
     
-    width: 440px;
-    height: 53rem;
-    overflow-y: auto;
+    width: 30%;
+    height: 100%;
+    overflow: hidden;
 `
 
 export const Notih1 = styled.h1`
     width: 100%;
+    //height: 10%;
     font-family: 'Kanit', sans-serif;
     font-style: normal;
     font-weight: 600;
     font-size: 20px;
-    
+    background: white;
     margin-bottom: 20px;
-
+    border-radius: 0.5rem;
     text-align: center;
     color: #555555;
     //border: 1px solid black;
     &:hover{
         cursor: default;
         user-select: none;
+    }
+    transition: background-color 0.2s ;
+
+    @media (max-width:1575px){
+        font-size: 18px;
+    }
+    @media (max-width:1200px){
+        font-size: 16px;
     }
 `
 
@@ -227,9 +261,13 @@ export const NotificationListContainer = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     width: 100%;
-    //height: 100vh;
-    overflow-y: auto;
-
+    height: 100%;
+    overflow-y: auto;   
+    padding: 10px;
+    border-radius: 0.5rem;
+    -moz-box-shadow:    inset 0 0 10px #f9f9f9;
+    -webkit-box-shadow: inset 0 0 10px #f9f9f9;
+    box-shadow:         inset 0 0 10px #f9f9f9;
     
     /* background-color: blue;
     border: 1px solid white; */
@@ -238,19 +276,16 @@ export const NotificationListContainer = styled.div`
     width: 0px;
     height: 0px;
     }
+    transition: background-color 0.2s ;
 `
 
 export const DivBox = styled.div`
     padding: 10px 10px 0px 15px;
-    
-    /* display: flex;
-    flex-direction: column;
-    justify-content: flex-start; */
-    //width: 100%;
+
     font-family: 'Kanit', sans-serif;
     font-style: normal;
     margin-bottom: 5px;
-
+    background: white;
     //text-align: center;
     color: white;
     border-radius: 20px;
@@ -276,15 +311,32 @@ export const DivBox = styled.div`
 export const DateP = styled.p`
     font-size: 12px;
     color: #a9a9a9;
+    @media (max-width:1575px){
+        font-size: 10px;
+    }
+    @media (max-width:1200px){
+        font-size: 8px;
+    }
 `
 export const TitleP = styled.h4`
     font-size: 20px;
     color: #454545;
+    @media (max-width:1575px){
+        font-size: 18px;
+    }
+    @media (max-width:1200px){
+        font-size: 16px;
+    }
 `
 export const DetailP = styled.p`
     font-size: 15px;
     color: #454545;
-    
+    @media (max-width:1575px){
+        font-size: 13px;
+    }
+    @media (max-width:1200px){
+        font-size: 11px;
+    }
 `
 
-
+////////////////////////////////////////////////
