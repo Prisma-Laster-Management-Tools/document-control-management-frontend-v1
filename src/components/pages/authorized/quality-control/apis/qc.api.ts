c9e57dab17a9d6b70861105c71ccf623a458a9a7
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-11 01:40:15 +0700

    update: finished sending product to queue

---
+++ src/components/pages/authorized/quality-control/apis/qc.api.ts
@@ -0,0 +1,10 @@
+import { AxiosPromise } from 'axios';
+import API_instance from '../../../../../core/axios/instance';
+import { transformDataFromAxiosPromiseToReadableFormat } from '../../../../../core/axios/response-mapper';
+import { ISendProductToControlQueueDTO } from '../shared/qc.interface';
+
+export async function API_SendProductToControlQueue(data: ISendProductToControlQueueDTO) {
+    const axios_promise: Promise<AxiosPromise> = API_instance.post('/api/quality-control/queue', data); // fetch all with no pagination
+    const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
+    return mapped_response;
+}
