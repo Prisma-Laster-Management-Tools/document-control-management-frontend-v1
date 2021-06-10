861f23d5bec063e7b52d041f775bf8c44f77e08c
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-11 00:55:06 +0700

    created shared toast-option

---
+++ src/shared/options/toast.option.ts
@@ -0,0 +1,11 @@
+import { ToastOptions } from 'react-toastify';
+
+export const ERROR_TOAST_OPTION: ToastOptions = {
+    position: 'bottom-center',
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    //progress: undefined,
+};
