15e094afb09088ac38aba544428cf6fe5b0f18be
unknown <tanawatt2541@gmail.com>
2021-06-12 21:58:26 +0700

    updated: Part detail page styles

--- src/components/pages/authorized/Purchase/views/PartDetail/views/Partdetail.tsx
+++ src/components/pages/authorized/Purchase/views/PartDetail/views/Partdetail.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Input, Divider} from 'antd';
+import Navbar from '../../../../../../common/navbar'
+import { PDBoxContainer, PDBtn, PDFormItem, PDListContainer, PDMainContainer, PDTextHeader, PDTextList } from './Partdetail.styles'
 
 export default function Partdetail() {
     return (
         <>
+            <Navbar/>
+            <PDMainContainer>
+                <PDBoxContainer>
+                    <PDTextHeader>Part List</PDTextHeader>
+                    <Divider/>
+                    <PDListContainer>
+                        <PDTextList>Text 1</PDTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <PDFormItem>
+                            <Input></Input>
+                        </PDFormItem>
+                    </PDListContainer>
+                    <PDListContainer>
+                        <PDTextList>Text 2</PDTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <PDFormItem>
+                            <Input></Input>
+                        </PDFormItem>
+                    </PDListContainer>
+
+                    <PDBtn>ยืนยัน</PDBtn>
+                </PDBoxContainer>
+            </PDMainContainer>
         </>
     )
 }
