f2870c1ca06a6661592eeb12ab82010ea45656c6
unknown <tanawatt2541@gmail.com>
2021-06-12 20:39:14 +0700

    updated: Create Resource Page styles

--- src/components/pages/authorized/Purchase/views/CreateSource/Createsource.tsx
+++ src/components/pages/authorized/Purchase/views/CreateSource/Createsource.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Input, Divider} from 'antd';
+import Navbar from '../../../../../common/navbar'
+import { CRTextHeader, CRBtn, CRFormItem2, CRBoxContainer, CreateResourceMainContainer, CRFormItem, CRListContainer, CRSelect, CRTextList } from './Createsource.styles'
 
 export default function Createsource() {
     return (
         <>
-        
+            <Navbar/>
+            <CreateResourceMainContainer>
+                <CRBoxContainer>
+                    <CRTextHeader>Lorem ipsum dolor sit amet.</CRTextHeader>
+                    <Divider/>
+                    <CRListContainer>
+                        <CRTextList>Part Number</CRTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <CRSelect></CRSelect>
+                        <CRFormItem>
+                            <Input></Input>
+                        </CRFormItem>
+                    </CRListContainer>
+                    <CRListContainer>
+                        <CRTextList>Company</CRTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <CRFormItem2>
+                            <Input></Input>
+                        </CRFormItem2>
+                    </CRListContainer>
+                    <CRListContainer>
+                        <CRTextList>E-mail</CRTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <CRFormItem2>
+                            <Input></Input>
+                        </CRFormItem2>
+                    </CRListContainer>
+                    <CRListContainer>
+                        <CRTextList>Seller</CRTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <CRFormItem2>
+                            <Input></Input>
+                        </CRFormItem2>
+                    </CRListContainer>
+                    <CRListContainer>
+                        <CRTextList>Commercial number</CRTextList>
+                        <Divider type="vertical" style={{height:"2rem"}}></Divider>
+                        <CRFormItem2>
+                            <Input></Input>
+                        </CRFormItem2>
+                    </CRListContainer>
+
+                    <CRBtn>ยืนยัน</CRBtn>
+                </CRBoxContainer>
+            </CreateResourceMainContainer>
         </>
     )
 }
