1594c9f674713236d13905757d463fcb08a9e500
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-13 23:21:02 +0700

    finished add feature of the purchasemen source

--- src/components/pages/authorized/Purchasement/views/SourceDetail/SourceDetail.tsx
+++ src/components/pages/authorized/Purchasement/views/SourceDetail/SourceDetail.tsx
@@ -1,13 +1,161 @@
-import { Button, PageHeader,Space,Table, Tooltip } from 'antd'
-import React from 'react'
+import { Button, Form, Input, PageHeader,Popover,Select,Space,Table, Tooltip } from 'antd'
+import React, { useState,useCallback } from 'react'
 import { MainContainer } from './sourceDetail.styles'
 import useSourceDetail from './useSourceDetail'
 import { EditFilled,DeleteFilled,PlusOutlined} from '@ant-design/icons';
 import { ConfirmationModalRequired } from '../../../../../../utilities/react-confirm-pro';
 import { IPurchasementSoruce } from '../../shared/interfaces/purchasement.interfaces';
 const {Column} = Table
+const {useForm} = Form
+const { Option } = Select;
+const formItemLayout = {
+    labelCol: {
+      xs: { span: 12 },
+      sm: { span: 9 },
+    },
+    wrapperCol: {
+      xs: { span: 12 },
+      sm: { span: 15 },
+    },
+  };
 const SourceDetail:React.FC = () => {
     const $hook_source_detail = useSourceDetail()
+    const [popOverVisible,setPopoverVisible] = useState(false)
+    const [form] = useForm()
+
+    const GET_COMMERCIAL_NUMBER_LIST = useCallback(
+        () => {
+            if(!$hook_source_detail.get.sourceDetail) return []
+            return $hook_source_detail.get.sourceDetail.map(data => data.commercial_number)
+        },
+        [$hook_source_detail.get.sourceDetail],
+    )
+    //
+    // ─── FOR POPOVER CONTENT ────────────────────────────────────────────────────────
+    //
+    const rendered_popover_content = <div style={{ fontFamily:'Kanit',width:'400px', }}>
+    <div style={{ background:'white' }}>
+        <h3 style={{ marginBottom:40 }}>สร้างแหล่งการจัดซื้อ</h3>
+        <Form onFinish={$hook_source_detail.events.onCreateSource.bind(null,form,() => setPopoverVisible(false))} form={form} {...formItemLayout}>
+            <Form.Item
+                    // style={{ width:"50%" }}
+                    name="part_number"
+                    label="วัสดุ"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'กรุณาระบุรหัสวัสดุ (Ex. RM00X)',
+                    },
+                    ]}
+                >
+                <Select
+                    showSearch
+                    style={{ width: 200 }}
+                    placeholder="เลือกวัสดุ"
+                    optionFilterProp="children"
+                    // onChange={onChange}
+                    // onFocus={onFocus}
+                    // onBlur={onBlur}
+                    // onSearch={onSearch}
+                    filterOption={(input, option) => {
+                        return option!.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                    }
+                >
+                    { $hook_source_detail.get.partDetail.map((pDetail) => {
+                        return  <Option value={pDetail.part_number}>{pDetail.part_number} - {pDetail.part_name}</Option>
+                    })}
+                </Select>
+            </Form.Item>
+            <Form.Item
+                    // style={{ width:"50%" }}
+                    name="commercial_number"
+                    label="รหัสการสั่งซื้อ"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'กรุณาระบุรหัสการสั่งซื้อ',
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                            if(!GET_COMMERCIAL_NUMBER_LIST().includes(value)){
+                                // check length can be done easiy other way
+                                return Promise.resolve()
+                            }
+                          return Promise.reject(new Error('เลขสั่งซื้อนี้มีอยู่แล้วในระบบ'));
+                        },
+                      })
+                    ]}
+                >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                    // style={{ width:"50%" }}
+                    name="company"
+                    label="บริษัท"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'กรุณาระบุบริษัท',
+                    },
+                    ]}
+                >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                    // style={{ width:"50%" }}
+                    name="seller"
+                    label="ผู้ขาย"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'กรุณาระบุชื่อผู้ขาย',
+                    },
+                    ]}
+                    tooltip="ชื่อผู้ขาย"
+                >
+                <Input />
+            </Form.Item>
+
+            
+            <Form.Item
+                    // style={{ width:"50%" }}
+                    name="email"
+                    label="อีเมล์"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'กรุณาระบุอีเมล์',
+                    },
+                    {
+                        type:'email',
+                        message: 'รูปแบบของอีเมล์ไม่ถูกต้อง'
+                    }
+                    ]}
+                    tooltip="สำคัญ: ใช้ในการสั่งซื้อโดยระบบอัตโนมัติ"
+                >
+                <Input />
+            </Form.Item>
+
+           
+
+            <Form.Item wrapperCol={{ sm:{span:15,offset:9} }}>
+                    <Button type="primary" htmlType="submit" block>
+                        ยืนยัน
+                    </Button>
+                    {/* <Button onClick={onCancel} danger block>
+                        ยกเลิก
+                    </Button> */}
+            </Form.Item>
+
+        </Form>
+    </div>
+</div>
+    // ────────────────────────────────────────────────────────────────────────────────
+
+
     return (
         <MainContainer>
             <div className="site-page-header-ghost-wrapper">
@@ -17,6 +165,19 @@ const SourceDetail:React.FC = () => {
                 title="ข้อมูลแหล่งการจัดซื้อ"
                 >
                 </PageHeader>
+            </div>
+            <div style={{ display:'flex',justifyContent:'flex-end',marginBottom:20,paddingRight:10 }}>
+
+            <Popover
+                content={rendered_popover_content}
+                // title="สร้างส่วนประกอบ"
+                trigger="click"
+                placement="left"
+                visible={popOverVisible}
+                onVisibleChange={(visible) => setPopoverVisible(visible)}
+            >
+                <Button type="primary" shape="circle" icon={<PlusOutlined />} size="middle" />
+            </Popover>
             </div>
             <Table onRow={(r) => ({onClick: () => console.log("lol")})} dataSource={ $hook_source_detail.get.sourceDetail || []} rowKey="id" size="middle" pagination={{ pageSize:8 }} bordered loading={$hook_source_detail.get.sourceDetail===null}>
                     <Column title="รหัสการสั่งซื้อ" dataIndex="commercial_number" key="commercial_number" />
--- src/components/pages/authorized/Purchasement/views/SourceDetail/useSourceDetail.tsx
+++ src/components/pages/authorized/Purchasement/views/SourceDetail/useSourceDetail.tsx
@@ -1,9 +1,13 @@
+import { FormInstance } from 'antd'
 import React, { useEffect, useState } from 'react'
-import { API_GetAllSourceDetail } from '../../apis/purchasement.api'
-import { IPurchasementSoruce } from '../../shared/interfaces/purchasement.interfaces'
+import { toast } from 'react-toastify'
+import { ERROR_TOAST_OPTION } from '../../../../../../shared/options/toast.option'
+import { API_CreateSourceDetail, API_GetAllPartDetail, API_GetAllSourceDetail } from '../../apis/purchasement.api'
+import { IPurchasementPartDetail, IPurchasementSoruce } from '../../shared/interfaces/purchasement.interfaces'
 
 export default function useSourceDetail() {
     const [sourceDetail,setSourceDetail] = useState<Array<IPurchasementSoruce> | null>(null)
+    const [partDetail,setPartDetail] = useState<Array<IPurchasementPartDetail>>([])
     async function getAllSourceDetail(){
         const mapped_response = await API_GetAllSourceDetail()
         if(mapped_response.success){
@@ -13,19 +17,51 @@ export default function useSourceDetail() {
             // failed to fetch
         }
     }
+    async function getAllPartDetail(){
+        const mapped_response = await API_GetAllPartDetail()
+        if(mapped_response.success){
+            setPartDetail(mapped_response.data)
+        }else{
+            //failed to fetch
+        }
+    }
+
+    async function onCreateSource(form:FormInstance<any>,$cb?:Function){
+        const postData = form.getFieldsValue()
+        const mapped_response = await API_CreateSourceDetail(postData)
+        if(mapped_response.success){
+            form.resetFields()
+            setSourceDetail(prevState => ([...prevState!,mapped_response.data]))
+            toast.success('สร้างข้อมูลแหล่งจัดซื้อสำเร็จ',ERROR_TOAST_OPTION);
+            if($cb) $cb() // hide the popover
+        }else{
+            //failed to create
+            if(mapped_response.data.message === "Key already exist"){
+                // Serial number already taken
+                toast.error('รหัสการสั่งซื้อนี้มีอยู่แล้วในระบบ',ERROR_TOAST_OPTION);
+            }else{
+                toast.error('เกิดข้อผิดพลาดในการสร้างแหล่งจัดซื้อ',ERROR_TOAST_OPTION);
+            }
+        }
+    }
     
     useEffect(() => {
         //On mount
         getAllSourceDetail()
+        getAllPartDetail()
     },[])
 
     // MAPPED USAGE
     return {
         get: {
             sourceDetail,
+            partDetail
         },
         set: {
             setSourceDetail
+        },
+        events: {
+            onCreateSource
         }
     }
 }
