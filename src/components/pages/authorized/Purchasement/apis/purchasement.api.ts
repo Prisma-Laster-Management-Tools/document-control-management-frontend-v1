74030f857a7c0c717beb010edf83205c44bfee1c
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-13 22:05:03 +0700

    created CR<|U|>D of the purchasement & also its related type declaration

--- src/components/pages/authorized/Purchasement/apis/purchasement.api.ts
+++ src/components/pages/authorized/Purchasement/apis/purchasement.api.ts
@@ -1,7 +1,7 @@
 import { AxiosPromise } from 'axios';
 import API_instance from '../../../../../core/axios/instance';
 import { transformDataFromAxiosPromiseToReadableFormat } from '../../../../../core/axios/response-mapper';
-import { ICreatePartDetailDTO } from '../shared/interfaces/purchasement.interfaces';
+import { ICreatePartDetailDTO, ICreateSourceDetailDTO } from '../shared/interfaces/purchasement.interfaces';
 
 export async function API_GetAllPartDetail() {
     const axios_promise: Promise<AxiosPromise> = API_instance.get('/api/purchasement/part-detail');
@@ -17,6 +17,24 @@ export async function API_CreatePartDetail(data: ICreatePartDetailDTO) {
 
 export async function API_RemovePartDetail(part_number: string) {
     const axios_promise: Promise<AxiosPromise> = API_instance.delete('/api/purchasement/remove-part-detail/' + part_number);
+    const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
+    return mapped_response;
+}
+
+export async function API_GetAllSourceDetail() {
+    const axios_promise: Promise<AxiosPromise> = API_instance.get('/api/purchasement/source');
+    const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
+    return mapped_response;
+}
+
+export async function API_CreateSourceDetail(data: ICreateSourceDetailDTO) {
+    const axios_promise: Promise<AxiosPromise> = API_instance.post('/api/purchasement/create-source-detail', data);
+    const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
+    return mapped_response;
+}
+
+export async function API_RemoveSourceDetail(id: number) {
+    const axios_promise: Promise<AxiosPromise> = API_instance.delete('/api/purchasement/remove-source-detail/' + id);
     const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
     return mapped_response;
 }
--- src/components/pages/authorized/Purchasement/shared/interfaces/purchasement.interfaces.ts
+++ src/components/pages/authorized/Purchasement/shared/interfaces/purchasement.interfaces.ts
@@ -12,3 +12,22 @@ export interface ICreatePartDetailDTO {
     part_name: string;
     part_description: string;
 }
+
+export interface IPurchasementSoruce {
+    id: number;
+    commercial_number: string;
+    part_number: string;
+    company: string;
+    email: string;
+    seller: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface ICreateSourceDetailDTO {
+    part_number: string;
+    company: string;
+    email: string;
+    seller: string;
+    commercial_number: string;
+}
