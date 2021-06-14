dbdb9830c833dc7ccea5262e008968268dcd706a
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-14 23:46:17 +0700

    used -> createAdapterBasedOnAntdLayout ->for purchasement layout

--- src/components/pages/authorized/Purchasement/adapter/PurchasementLayout/PurchasementLayout.tsx
+++ src/components/pages/authorized/Purchasement/adapter/PurchasementLayout/PurchasementLayout.tsx
@@ -1,80 +1,29 @@
-import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-import Navbar from '../../../../../common/navbar';
-import { ANTD_LayoutWrapper, CategoryLabelSider } from '../../../../../../shared/styles/common';
+
+import React from 'react'
+import {createAdapterBasedOnAntdLayout} from "../../../../../../core/hoc/createAdapterBasedOnAntdLayout"
 import PartDetail from '../../views/PartDetail';
 import SourceDetail from '../../views/SourceDetail';
-
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-
-interface IState {
-    collapsed: boolean;
-    action: 'part-detail' | 'source' | 'request';
-}
-interface IProps {}
-
-export default class PurchasementLayout extends Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            collapsed: false,
-            action: 'part-detail',
-        };
+import PurchasementRequest from '../../views/PurchasementRequest';
+import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+const PurchasementLayout = createAdapterBasedOnAntdLayout({title:"การสั่งซื้อ",default_fragment:'part-detail',fragment_data:[
+    {
+        component: PartDetail,
+        name: "part-detail",
+        proper_label: "ข้อมูลส่วนประกอบ/วัสดุ",
+        icon: DesktopOutlined
+    },
+    {
+        component: SourceDetail,
+        name: "source-detail",
+        proper_label: "แหล่งการจัดซื้อ",
+        icon: DesktopOutlined
+    },
+    {
+        component: PurchasementRequest,
+        name: "request",
+        proper_label: "คำสั่งซื้อ",
+        icon: DesktopOutlined
     }
+]})
 
-    onCollapse = (collapsed: any) => {
-        this.setState({ collapsed });
-    };
-
-    render() {
-        const { collapsed, action } = this.state;
-        let rendered_content = null;
-        switch (action) {
-            case 'part-detail':
-                rendered_content = <PartDetail/>;
-                break;
-
-            case 'source':
-                rendered_content = <SourceDetail/>;
-                break;
-
-            case 'request':
-                rendered_content = null;
-                break;
-
-            default:
-                break;
-        }
-        return (
-            <ANTD_LayoutWrapper>
-                 <Layout style={{ height:"100%" }}>
-                 <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-                 <CategoryLabelSider>
-                     การสังซื้อ
-                 </CategoryLabelSider>
-                 <Menu onSelect={(info) => this.setState({action:info.key as IState["action"]})} theme="dark" selectedKeys={[action]} defaultSelectedKeys={['product-detail']} mode="inline">
-                 <Menu.Item key="part-detail" icon={<PieChartOutlined />}>
-                     ข้อมูลส่วนประกอบ/วัสดุ
-                 </Menu.Item>
-                 <Menu.Item key="source" icon={<DesktopOutlined />}>
-                     แห่ลงการจัดซื้อ
-                 </Menu.Item>
-                 </Menu>
-             </Sider>
-             <Layout className="site-layout">
-                 <Header style={{ padding: 0 }}><Navbar/></Header>
-                 <Content style={{ margin: '0 16px' }}>
-
-                     <div className="site-layout-background" style={{ padding: 0, minHeight: 320,maxHeight: '100%',overflowX:'hidden',overflowY:'auto' }}>
-                         {rendered_content}
-                     </div>
-                 </Content>
-                 {/* <Footer style={{ textAlign: 'center' }}>Design by AAW0KENN CREWS</Footer> */}
-             </Layout>
-             </Layout>
-            </ANTD_LayoutWrapper>
-         );
-    }
-}
+export default PurchasementLayout
