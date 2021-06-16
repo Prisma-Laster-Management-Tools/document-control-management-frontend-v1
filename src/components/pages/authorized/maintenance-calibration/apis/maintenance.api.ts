05437b294827999cbb11bc02f58d08c60f3a985f
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-16 22:44:20 +0700

    created (api)API_GetAllMaintenanceCycleData

---
+++ src/components/pages/authorized/maintenance-calibration/apis/maintenance.api.ts
@@ -0,0 +1,9 @@
+import { AxiosPromise } from 'axios';
+import API_instance from '../../../../../core/axios/instance';
+import { transformDataFromAxiosPromiseToReadableFormat } from '../../../../../core/axios/response-mapper';
+
+export async function API_GetAllMaintenanceCycleData() {
+    const axios_promise: Promise<AxiosPromise> = API_instance.get('/api/maintenance/');
+    const mapped_response = await transformDataFromAxiosPromiseToReadableFormat(axios_promise, { on_success: '*', on_fail: '*' });
+    return mapped_response;
+}
