fedf159d2b8290c48453fdc88a5589e1316af07c
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-18 23:07:18 +0700

    created (fn)createStaticQcChartTypeWithPassedData

--- src/components/pages/authorized/Dashboard/views/Dashboard/useDashboard.tsx
+++ src/components/pages/authorized/Dashboard/views/Dashboard/useDashboard.tsx
@@ -3,51 +3,7 @@ import { TRoles } from './Dashboard'
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { API_GetQcStatistic } from '../../apis/dashboard.api';
 import { IQcStatisticData } from '../../shared/interfaces/dashboard.interfaces';
-
-// const format = [
-//     {
-//       name: 'Page A',
-//       uv: 4000,
-//       pv: 2400,
-//       amt: 2400,
-//     },
-//     {
-//       name: 'Page B',
-//       uv: 3000,
-//       pv: 1398,
-//       amt: 2210,
-//     },
-//     {
-//       name: 'Page C',
-//       uv: 2000,
-//       pv: 9800,
-//       amt: 2290,
-//     },
-//     {
-//       name: 'Page D',
-//       uv: 2780,
-//       pv: 3908,
-//       amt: 2000,
-//     },
-//     {
-//       name: 'Page E',
-//       uv: 1890,
-//       pv: 4800,
-//       amt: 2181,
-//     },
-//     {
-//       name: 'Page F',
-//       uv: 2390,
-//       pv: 3800,
-//       amt: 2500,
-//     },
-//     {
-//       name: 'Page G',
-//       uv: 3490,
-//       pv: 4300,
-//       amt: 2100,
-//     },
-//   ]
+import { createStaticQcChartTypeWithPassedData } from './staticChartRenderer';
 
 export default function useDashboard() {
     const [focusedRole,setFocusRole] = useState<TRoles>('qc')
@@ -99,50 +55,10 @@ export default function useDashboard() {
                     name: 'อยู่ในคิวการตรวจสอบ',
                     'อยู่ในคิวการตรวจสอบ': statistic.total_in_queue
                 }
-                // ,
-                // {
-                //     name: 'ยังไม่เคยได้รับการตรวจสอบ',
-                //     total: statistic.total_never_qc
-                // },
-                // {
-                //     name: 'ผ่านมาตรฐานการตรวจสอบ',
-                //     total: statistic.total_qc_passed
-                // },
-                // {
-                //     name: 'ไม่ผ่านมาตรฐานการตรวจสอบ',
-                //     total: 2
-                // }
-                // ,
-                // {
-                //     name: 'อยู่ในคิวการตรวจสอบ',
-                //     total: statistic.total_in_queue
-                // }
             ]
         }
                     
-      setChartElement(<BarChart
-        width={500}
-        height={300}
-        data={dataset}
-        margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-        }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="สินค้าทั้งหมด" stackId="1" fill="#8884d8" />
-            <Bar dataKey="ยังไม่เคยได้รับการตรวจสอบ" stackId="1" fill="gray" />
-            <Bar dataKey="ผ่านมาตรฐานการตรวจสอบ" stackId="1" fill="green" />
-            <Bar dataKey="ไม่ผ่านมาตรฐานการตรวจสอบ" stackId="1" fill="red" />
-            <Bar dataKey="อยู่ในคิวการตรวจสอบ" stackId="1" fill="orange" />
-            {/* <Bar dataKey="uv" stackId="a" fill="#82ca9d" /> */}
-        </BarChart>)
+      setChartElement(createStaticQcChartTypeWithPassedData(dataset))
     }
     // ────────────────────────────────────────────────────────────────────────────────
 
