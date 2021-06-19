4ef7a57aa99e5dffe1a83f5ba94b6ae4a8d6c391
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-19 19:06:45 +0700

    update: replace the new deliberation system in the deliberationLayout

--- src/components/pages/authorized/Deliberation/adapter/DeliberationLayout/DeliberationLayout.tsx
+++ src/components/pages/authorized/Deliberation/adapter/DeliberationLayout/DeliberationLayout.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import {createAdapterBasedOnAntdLayout} from "../../../../../../core/hoc/createAdapterBasedOnAntdLayout"
 import Sales from "../../views/Sales"
 import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-const DeliberationLayout = createAdapterBasedOnAntdLayout({title:"การขาย",default_fragment:'sales',fragment_data:[{
-    component: Sales,
-    name: "sales",
-    proper_label: "กิจกรรมการขาย",
+import ProductManufact from '../../views/ProductManufact';
+const DeliberationLayout = createAdapterBasedOnAntdLayout({title:"การขาย/จัดจำหน่าย",default_fragment:'prod_manufact',fragment_data:[{
+    component: ProductManufact,
+    name: "prod_manufact",
+    proper_label: "การจัดจำหน่าย",
     icon: DesktopOutlined
 }]})
 
