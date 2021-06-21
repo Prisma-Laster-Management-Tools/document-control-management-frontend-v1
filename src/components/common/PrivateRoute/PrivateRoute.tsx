d28e60688b90e3258ace62e033665daa02ec4353
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-21 10:58:41 +0700

    smoothen the PrivateRoute to have a delay of 100 milisecond to ensure that authState is already fetch

--- src/components/common/PrivateRoute/PrivateRoute.tsx
+++ src/components/common/PrivateRoute/PrivateRoute.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { authenticationState } from '../../../store/recoil/authentication/authentication.atom'
 
 import { Route, Redirect } from 'react-router-dom';
 const PrivateRoute = ({component: Component, ...rest}:any) => {
-const [authState,setAuthState] = useRecoilState(authenticationState)
+  const [authState,setAuthState] = useRecoilState(authenticationState)
+  const [readyToBeLoaded,setReadyToBeLoaded] = useState<boolean>(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setReadyToBeLoaded(true)
+    }, 100);
+  },[])
+  if(!readyToBeLoaded) return null
   return (
         <Route
             {...rest}
